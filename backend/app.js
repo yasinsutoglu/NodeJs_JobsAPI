b555a1b49abc7519a092065c557107e1625fb018
Yasin Sutoglu <ysutoglu@gmail.com>
2023-03-27 08:34:27 +0300

    swagger

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit')
 
+//Swagger 
+const swaggerUI =require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 //connectDB
 const connectDB = require('./db/connect')
 
@@ -28,6 +33,13 @@ app.use(rateLimiter({
   windowMs:15 * 60 * 1000,
   max:100 //limit each IP to 100 request per windowMs
 })); //!ne kadar miktarda request atılacağını belirler(limitler)
+
+app.get('/', (req,res)=>{
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+})
+
+app.use('./api-docs' , swaggerUI.serve , swaggerUI.setup(swaggerDocument))
+
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
