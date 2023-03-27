d5b22ede43d3fa419bc782ea5c4ea99c4d3e423a
Yasin Sutoglu <ysutoglu@gmail.com>
2023-03-27 09:01:31 +0300

    update

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -1,4 +1,4 @@
-const Job = require('../../models/Job');
+const Job = require('../models/Job');
 const {StatusCodes} = require('http-status-codes');
 const {BadRequestError , NotFoundError} = require('../../errors')
 
